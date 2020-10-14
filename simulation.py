52c694273d3b6e51f724aa2a0bfac4c74e53e7fe
maalekkk <bartek.malkowski97@gmail.com>
2020-10-14 22:57:43 +0200

    Add main definition

--- simulation.py
+++ simulation.py
@@ -5,3 +5,11 @@ init_pos_limit = 10.0
 
 def distance(wolf, sheep):
     return math.sqrt(((sheep.x - wolf.x) ** 2) + ((sheep.y - wolf.y) ** 2))
+
+
+def main():
+    print('Wolf and sheep')
+
+
+if __name__ == "__main__":
+    main()
